718c6f78d28129f93d32bc9ab6cf085ce6e82dbe
mmeguizo <mark.meguizo@gmail>
2023-07-18 22:59:35 +0800

    update to tailwind not yet done

--- script.js
+++ script.js
@@ -1,104 +1,36 @@
 document.addEventListener('DOMContentLoaded', function() {
+  const wrapper = document.querySelector('.wrapper');
+  const loginLink = document.querySelector('.login-link');
+  const registerLink = document.querySelector('.register-link');
+  const btnPopup = document.querySelector('.btnLogin-popup');
+  const iconClose = document.querySelector('.icon-close');
 
-const wrapper  = document.querySelector('.wrapper');
-const loginLink  = document.querySelector('.login-link');
-const registerLink  = document.querySelector('.register-link');
-
-const btnPopup = document.querySelector('.btnLogin-popup')
-const iconClose = document.querySelector('.icon-close')
-
-registerLink.addEventListener('click', () => {
+  registerLink.addEventListener('click', () => {
     wrapper.classList.add('active');
-})
-loginLink.addEventListener('click', () => {
-    wrapper.classList.remove('active');
-})
-
-
-btnPopup.addEventListener('click', () => {
-    wrapper.classList.add('active-popup');
-})
-iconClose.addEventListener('click', () => {
-    wrapper.classList.remove('active-popup');
-})
-
-
-function isClickInsidePopup(event) {
-    return (
-        wrapper.contains(event.target) ||
-        btnPopup.contains(event.target)
-      );
-}
-
-document.addEventListener('click', function(event) {
-    if (!isClickInsidePopup(event)) {
-        wrapper.classList.remove('active-popup');
-    }
   });
 
+  loginLink.addEventListener('click', () => {
+    wrapper.classList.remove('active');
+  });
 
-console.log(wrapper);
+  btnPopup.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
+  });
 
-});
+  iconClose.addEventListener('click', () => {
+    wrapper.classList.remove('active-popup');
+  });
 
-/*
+  function isClickInsidePopup(event) {
+    return (
+      wrapper.contains(event.target) ||
+      btnPopup.contains(event.target)
+    );
+  }
 
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    const wrapper = document.querySelector('.wrapper');
-    const formBoxLogin = document.querySelector('.form-box.login');
-    const formBoxRegister = document.querySelector('.form-box.register');
-    const closeButton = document.querySelector('.icon-close');
-    const btnPopup = document.querySelector('.btnLogin-popup');
-    const loginLink = document.querySelector('.login-link');
-    const registerLink = document.querySelector('.register-link');
-  
-    function togglePopup() {
-      wrapper.classList.toggle('active-popup');
-    }
-  
-    function closePopup() {
+  document.addEventListener('click', function(event) {
+    if (!isClickInsidePopup(event)) {
       wrapper.classList.remove('active-popup');
     }
-  
-    function isClickInsidePopup(event) {
-      return (
-        formBoxLogin.contains(event.target) ||
-        formBoxRegister.contains(event.target) ||
-        btnPopup.contains(event.target)
-      );
-    }
-  
-    document.addEventListener('click', function(event) {
-      if (!isClickInsidePopup(event)) {
-        closePopup();
-      }
-    });
-  
-    closeButton.addEventListener('click', function() {
-      closePopup();
-    });
-  
-    btnPopup.addEventListener('click', function() {
-      togglePopup();
-    });
-  
-    loginLink.addEventListener('click', function(event) {
-      event.preventDefault();
-      formBoxRegister.style.transform = 'translateX(400px)';
-      formBoxLogin.style.transform = 'translateX(0)';
-    });
-  
-    registerLink.addEventListener('click', function(event) {
-      event.preventDefault();
-      formBoxLogin.style.transform = 'translateX(-400px)';
-      formBoxRegister.style.transform = 'translateX(0)';
-    });
   });
-  
-
-
-
-
-*/
+});
